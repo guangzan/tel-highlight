5d887528b009d27016dccaaaee48e79e2a212ad7
guangzan <guangzan1999@outlook.com>
2022-03-21 22:53:14 +0800

    style: format

--- test/highlight/middleHighlight.test.ts
+++ test/highlight/middleHighlight.test.ts
@@ -1,90 +1,90 @@
-import { telHighlight } from "../../src";
-import { test, expect, it } from "vitest";
+import { telHighlight } from '../../src'
+import { test, expect, it } from 'vitest'
 
-test("middle string tel and string highlight", () => {
+test('middle string tel and string highlight', () => {
   const input = {
-    tel: "12345678910",
-    highlight: "789",
-  };
+    tel: '12345678910',
+    highlight: '789',
+  }
   const output = [
     {
-      num: "123456",
+      num: '123456',
       isHighlight: false,
     },
     {
-      num: "789",
+      num: '789',
       isHighlight: true,
     },
     {
-      num: "10",
+      num: '10',
       isHighlight: false,
     },
-  ];
-  expect(telHighlight(input)).toEqual(output);
-});
+  ]
+  expect(telHighlight(input)).toEqual(output)
+})
 
-test("middle number tel and number highlight", () => {
+test('middle number tel and number highlight', () => {
   const input = {
     tel: 12345678910,
     highlight: 789,
-  };
+  }
   const output = [
     {
-      num: "123456",
+      num: '123456',
       isHighlight: false,
     },
     {
-      num: "789",
+      num: '789',
       isHighlight: true,
     },
     {
-      num: "10",
+      num: '10',
       isHighlight: false,
     },
-  ];
-  expect(telHighlight(input)).toEqual(output);
-});
+  ]
+  expect(telHighlight(input)).toEqual(output)
+})
 
-test("middle number tel and string highlight", () => {
+test('middle number tel and string highlight', () => {
   const input = {
     tel: 12345678910,
-    highlight: "789",
-  };
+    highlight: '789',
+  }
   const output = [
     {
-      num: "123456",
+      num: '123456',
       isHighlight: false,
     },
     {
-      num: "789",
+      num: '789',
       isHighlight: true,
     },
     {
-      num: "10",
+      num: '10',
       isHighlight: false,
     },
-  ];
-  expect(telHighlight(input)).toEqual(output);
-});
+  ]
+  expect(telHighlight(input)).toEqual(output)
+})
 
-test("middle string tel and number highlight", () => {
+test('middle string tel and number highlight', () => {
   const input = {
-    tel: "12345678910",
+    tel: '12345678910',
     highlight: 789,
-  };
+  }
   const output = [
     {
-      num: "123456",
+      num: '123456',
       isHighlight: false,
     },
     {
-      num: "789",
+      num: '789',
       isHighlight: true,
     },
     {
-      num: "10",
+      num: '10',
       isHighlight: false,
     },
-  ];
-  expect(telHighlight(input)).toEqual(output);
-});
+  ]
+  expect(telHighlight(input)).toEqual(output)
+})
